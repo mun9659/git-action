428db7983d53bcaf6dcb3f75c2406c77874a625e
Mun Kyoung-min <mun9659@naver.com>
2024-03-04 13:40:56 +0900

    project init

---
+++ src/main/java/study/gitwebhook/GitwebhookApplication.java
@@ -0,0 +1,13 @@
+package study.gitwebhook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitwebhookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitwebhookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/study/gitwebhook/GitwebhookApplicationTests.java
@@ -0,0 +1,13 @@
+package study.gitwebhook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitwebhookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
