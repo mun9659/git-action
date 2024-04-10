5dbb7072216d93c9283afcbbe0be03d2adf5bb14
Mun Kyoung-min <mun9659@naver.com>
2024-04-10 23:14:03 +0900

    Redis 진행 예정

--- src/main/java/study/gitwebhook/GitwebhookApplication.java
+++ src/main/java/study/gitwebhook/GitwebhookApplication.java
@@ -2,8 +2,10 @@ package study.gitwebhook;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class GitwebhookApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/study/gitwebhook/entity/redis/RefreshToken.java
@@ -0,0 +1,33 @@
+package study.gitwebhook.entity.redis;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.TimeToLive;
+import org.springframework.data.redis.core.index.Indexed;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
+@RedisHash(value = "refresh_token")
+public class RefreshToken {
+
+    @Id
+    private String authId;
+
+    @Indexed
+    private String token;
+
+    @TimeToLive
+    private long ttl;
+
+    public RefreshToken update(String token, long ttl) {
+        this.token = token;
+        this.ttl = ttl;
+        return this;
+    }
+
+}
